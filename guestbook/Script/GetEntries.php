c12a9a58eacb3f417d8f880022dfa12d05e01883
MyLittleVerdant <47346691+MyLittleVerdant@users.noreply.github.com>
2021-07-13 20:51:48 +0300

    Refactoring

--- guestbook/Script/GetEntries.php
+++ guestbook/Script/GetEntries.php
@@ -1,32 +1,19 @@
 <?php
 
-    
-    $host = 'localhost'; 
-    $database = 'guestbook'; 
-    $user = 'root';
-    $password = 'greenhood13'; 
 
-    $mysql=new PDO("mysql:host=localhost;dbname=guestbook",$user,$password); 
-   
+$host = 'localhost';
+$database = 'guestbook';
+$user = 'root';
+$password = 'greenhood13';
 
-    if(isset($_POST['Info']))
-    {
-        $query=$mysql->prepare("SELECT `UserName`,`E-mail`,`Homepage`,`DateTime`, `Text`FROM `entry`");
-        $query->execute();
-        
-        $get_entries=$query->fetchAll();
-
-        foreach ($get_entries as $row) {
-            $entry_list[]=$row; 
-          }
-
-        
-        
-        //echo json_encode($entry_list);
-        echo json_encode($get_entries);
-    }
-    
+$mysql = new PDO("mysql:host=localhost;dbname=guestbook", $user, $password);
 
 
+if (isset($_POST['Info'])) {
+    $query = $mysql->prepare("SELECT `UserName`,`E-mail`,`Homepage`,`DateTime`, `Text`FROM `entry`");
+    $query->execute();
 
-?>
+    $get_entries = $query->fetchAll();
+
+    echo json_encode($get_entries);
+}
